1473f16ca357635896254264a5d50071e3e97d9d
JusteBlanc <loic.blanc@hotmail.fr>
2016-03-04 12:53:34 +0100

    Correctif

--- src/android/com/mbppower/CameraPreview.java
+++ src/android/com/mbppower/CameraPreview.java
@@ -54,7 +54,8 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         //Check permission android 6.0
-        cordova.requestPermission(this, 0, { Manifest.permission.CAMERA , Manifest.permission.WRITE_EXTERNAL_STORAGE});
+        String[] permissions = { Manifest.permission.CAMERA , Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        cordova.requestPermission(this, 0, permissions);
         
 
     	if (setOnPictureTakenHandlerAction.equals(action)){
